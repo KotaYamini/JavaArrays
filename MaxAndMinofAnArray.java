1a5823a7008637aef40a850082b09ab043379560
yaminikota <yamini.kota466@gmail.com>
2023-04-30 22:56:29 +0530

    staticSumof1Darray

---
+++ MaxAndMinofAnArray.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class MaxAndMinofAnArray {
+    public static void main(String[] args) {
+        /*
+        * Take input an array A of size N and write a program to print maximum and minimum elements of the input.
+        *  The only line of the input would contain a single integer N that represents the length of the array followed by the N elements of the input array A.
+        * */
+
+        Scanner scn = new Scanner(System.in);
+        int N = scn.nextInt();
+        int A[] = new int[N];
+
+        for(int i=0; i<A.length; i++){
+            A[i] = scn.nextInt();
+        }
+
+        System.out.println(minArr(A) + " " + maxArr(A));
+    }
+
+    public static int minArr(int arr[]){
+        int N = arr.length;
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<N; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static int maxArr(int arr[]){
+        int N = arr.length;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<N; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return  max;
+    }
+}
--- out/production/JavaArrays/README.md
+++ out/production/JavaArrays/README.md
@@ -1,1 +1,4 @@
 # JavaArrays
+
+Sum of 1D Arrays
+![image](https://user-images.githubusercontent.com/48117959/235366026-bd589e43-d930-4f44-9324-6ef0fc91a0d2.png)
