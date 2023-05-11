7a3f2829b53120d8bb4338f347f1afc029ad000c
yaminikota <yamini.kota466@gmail.com>
2023-05-11 22:47:21 +0530

    OddNegativeIncrement

---
+++ beginnerContestThree/TwoDimensionArraySum.java
@@ -0,0 +1,15 @@
+package beginnerContestThree;
+
+/*
+* Given 2D Array A of size N*N return an array in which arr[i] have the sum of ith diagonal elements from 2D array
+*
+* NOTE
+* 1 Diagonal starting from 0th row or last column
+* 2 Diagonal direction Top Right Bottom left
+* */
+
+public class TwoDimensionArraySum {
+    public static void main(String[] args) {
+
+    }
+}
